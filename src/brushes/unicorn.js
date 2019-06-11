e9f54a447a90d8baab944cd7a4166528317666aa
FH <feli.hp@gmail.com>
2019-06-11 22:21:36 +0200

    array led bf 34

--- src/brushes/unicorn.js
+++ src/brushes/unicorn.js
@@ -82,19 +82,19 @@ var onLoaded = require('../onloaded.js');
         directionY.applyQuaternion(orientation);
         directionY.normalize();
 
-        directionx.set(0.08, 0, 0);
+        directionx.set(0.075, 0, 0);
         directionx.applyQuaternion(orientation);
         //directionx.normalize();
 
-        directiony.set(0, 0.05, 0);
+        directiony.set(0, 0.075, 0);
         directiony.applyQuaternion(orientation);
         //directiony.normalize();
 
         directionX.set(1, 0, 0);
         directionX.applyQuaternion(orientation);
         directionX.normalize();
-        pointerPosition.add(directiony.clone().multiplyScalar(-0.6));
-        pointerPosition.add(directionx.clone().multiplyScalar(-0.3));
+        pointerPosition.add(directiony.clone().multiplyScalar(-0.5));
+        pointerPosition.add(directionx.clone().multiplyScalar(-0.5));
 
         for (i = 0; i < BUFFERSIZEX; i++) {
 
