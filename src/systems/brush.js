d15ed8e1976ce6b4da0339065db1ec3b27054e6c
fhernand <fhernand@users.noreply.github.com>
2019-06-16 13:12:01 +0200

    remove spacing constraint

--- src/systems/brush.js
+++ src/systems/brush.js
@@ -106,7 +106,8 @@ AFRAME.registerBrush = function (name, definition, options) {
 
   function wrapAddPoint (addPointMethod) {
     return function addPoint (position, orientation, pointerPosition, pressure, timestamp) {
-      if ((this.data.prevPosition && this.data.prevPosition.distanceTo(position) <= this.options.spacing) ||
+      if (
+	      //(this.data.prevPosition && this.data.prevPosition.distanceTo(position) <= this.options.spacing) ||
           this.options.maxPoints !== 0 && this.data.numPoints >= this.options.maxPoints) {
         return;
       }
